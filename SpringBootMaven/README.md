49c69594fdb5c70d11260a39cfd335ba6f61b782
Admin <you@example.com>
2023-04-25 22:24:05 +0530

    first commit

---
+++ SpringBootMaven/README.md
@@ -0,0 +1,7 @@
+# SpringBootMaven
+
+
+http://localhost:8080/swagger-ui.html
+
+
+`./mvnw compile flyway:migrate`
---
+++ SpringBootMaven/src/main/java/com/myAPI/SpringBootMaven/entity/User.java
@@ -0,0 +1,91 @@
+package com.myAPI.SpringBootMaven.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @SequenceGenerator(name = "user_id_generator", sequenceName = "user_id_seq")
+    @GeneratedValue(generator = "user_id_generator")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @JsonProperty("created_at")
+    private LocalDateTime createdAt;
+
+    @JsonProperty("updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    void preSave() {
+        if(createdAt == null) {
+            createdAt = LocalDateTime.now();
+        }
+    }
+
+    @PreUpdate
+    void preUpdate() {
+        if(updatedAt == null) {
+            updatedAt = LocalDateTime.now();
+        }
+    }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+    
+    
+}
